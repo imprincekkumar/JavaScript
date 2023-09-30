447ac54e2a832a34cdce8a91d6820d28b541a862
imprincekkumar <80305084+imprincekkumar@users.noreply.github.com>
2023-09-30 16:10:57 +0530

    dataTypes Conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,56 @@
+let score = 33
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+/* gives output NaN:not a  number 
+
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);
+
+*/
+
+/* if thers a numerical value inside a string it can be converted into number data type 
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+/* if there is a mixture of number and string value inside a string it will throw an error NaN
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+
+*/
+
+/* A number can be converted into string
+
+let valueInNumber = String(score)
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+*/
+
+/* A number can be converted into a boolean value
+
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+*/
+
+/* When An empty string is converted into boolean it gives false    
+let isLoggedIn="";
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+*/
+
+/*When a string is converted to boolean it gives true
+let isLoggedIn="Prince";
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+*/
+
